34a2df77eece5be5f27a467b584434ab4bd2d247
André Gomes da Silva <67976938+andregds85@users.noreply.github.com>
2021-02-10 19:00:57 -0300

    teste
    teste

---
+++ cmd.php
@@ -0,0 +1,55 @@
+
+php artisan migrate:reset
+
+
+php artisan make:seeder PermissionTableSeeder
+
+
+php artisan db:seed --class=PermissionTableSeeder
+
+
+php artisan make:seeder CreateAdminUserSeeder
+
+
+php artisan db:seed --class=CreateAdminUserSeeder
+
+
+Criar uma Model e Migrations 
+php artisan make:model nome da Model -m
+
+
+Criar um Controller 
+php artisan make:controller PostController --resource
+
+
+
+Criando um modulo
+https://www.diegobrocanelli.com.br/php/laravel-module/
+
+
+
+Criando a autenticação toda.
+https://medium.com/@tm.talhamaqsood18/simple-authentication-in-laravel-8-9-0-step-by-step-with-images-a63149679f2b
+
+
+
+
+
+	
+id
+categorias_id
+solicitacao
+cns
+nomedousuario
+municipio
+datasolicitacao
+unidadedesejada
+codigo
+observacao1
+observacao2
+observacao3
+observacao4
+observacao5
+statusSolicitacao
+created_at
+updated_at
